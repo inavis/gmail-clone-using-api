09163304a57256bb3b07e80b8b438af1cc2e748b
Sivani Saravanan <31069721+inavis@users.noreply.github.com>
2021-11-26 16:50:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 console.log("console");
 
-let userid;
-let username;
-let userimage;
-let useremail;
+// let userid;
+// let username;
+// let userimage;
+// let useremail;
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    userid=profile.getId();
-    username=profile.getName();
-    userimage=profile.getImageUrl();
-    useremail = profile.getEmail();
-    console.log(userid);
-    console.log(username);
-    console.log(userimage);
-    console.log(useremail); 
-    let url1 =`https://gmail.googleapis.com/gmail/v1/users/${userid}/profile`;
-    console.log(url1);
-    fetch(url1)
-    .then((response)=> response.json())
-    .then ((data)=>console.log(data));
-  }
+// function onSignIn(googleUser) {
+//     var profile = googleUser.getBasicProfile();
+//     userid=profile.getId();
+//     username=profile.getName();
+//     userimage=profile.getImageUrl();
+//     useremail = profile.getEmail();
+//     console.log(userid);
+//     console.log(username);
+//     console.log(userimage);
+//     console.log(useremail); 
+   
+//   }
   
-  function signOut() {
-      var auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then(function () {
-        console.log('User signed out.');
-      });
-    }
+//   function signOut() {
+//       var auth2 = gapi.auth2.getAuthInstance();
+//       auth2.signOut().then(function () {
+//         console.log('User signed out.');
+//       });
+//     }
 
 
 
